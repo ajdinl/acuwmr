2062dfa5af6696e23e605ffd22a5f1f70ae15ba1
ajdinl <ajdinl@live.com>
2024-02-13 10:18:07 +0100

    update nodemailer

--- app.js
+++ app.js
@@ -3,51 +3,46 @@ const nodemailer = require('nodemailer')
 const cpu = osu.cpu
 require('dotenv').config()
 
-
 setInterval(print, 3000)
 setInterval(main, 3000)
 
 let cpuUsage = 0
 let memoryUsage = 0
 
-function print(){
-  cpu.usage()
-  .then(info => {
-      cpuUsage = 'CPU average usage in percentage: ' + info + ' '
-      console.log(cpuUsage)
-    })
-    
-    const mem = osu.mem
-    mem.used()
-    .then(info => {
-      memoryUsage = info
-      console.log(memoryUsage)
-    })
-  }
-  
-  function main(){
+function print() {
+  cpu.usage().then((info) => {
+    cpuUsage = 'CPU average usage in percentage: ' + info + ' '
+    console.log(cpuUsage)
+  })
 
+  const mem = osu.mem
+  mem.used().then((info) => {
+    memoryUsage = info
+    console.log(memoryUsage)
+  })
+}
+
+function main() {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.DB_MAIL,
-      pass: process.env.DB_PASSWORD
-    }
+      pass: process.env.DB_PASSWORD,
+    },
   })
 
   const mailOptions = {
     from: process.env.DB_MAIL,
     to: process.env.DB_TO_MAIL,
     subject: 'Cpu and memory usage',
-    text: cpuUsage + JSON.stringify(memoryUsage)
+    text: cpuUsage + JSON.stringify(memoryUsage),
   }
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-    console.log(error);
+      console.log(error)
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log('Email sent: ' + info.response)
     }
-
   })
 }
